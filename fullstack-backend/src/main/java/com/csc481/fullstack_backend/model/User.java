2c5bb817f9023e0ec6dd94bbe0fb2d0cdb4a753a
Avry Jackson <158083847+avryjacksoncom@users.noreply.github.com>
2025-02-27 16:27:05 -0800

    Project start for main, has the frontend files and backend files. Still in process of developing the backend components and front end design. We will work on that.

---
+++ fullstack-backend/src/main/java/com/csc481/fullstack_backend/model/User.java
@@ -0,0 +1,11 @@
+
+package main.java.com.csc481.fullstack_backend.model;
+import javax.persistence.Entity;
+
+public class User {
+    private Long reservationId;
+    private String firstName;
+    private String phoneNumber;
+    
+    
+}
---
+++ fullstack-backend/src/test/java/com/csc481/fullstack_backend/FullstackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc481.fullstack_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
