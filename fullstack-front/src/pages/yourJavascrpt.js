d9685da7891311f029a439019a02adc51b8f05e3
Avry Jackson <158083847+avryjacksoncom@users.noreply.github.com>
2025-03-14 17:55:14 -0700

    comit

---
+++ fullstack-front/src/pages/yourJavascrpt.js
@@ -0,0 +1,10 @@
+import React from 'react'
+export default function yourJavascript() {
+
+  return (
+    // Your HTML code would be here. were returning html
+    <div className="container">
+      
+      </div>
+  );
+}
