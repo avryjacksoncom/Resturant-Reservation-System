fc36a372c0203a068657c96d0d48d12f2f3bbd64
Robert <robertosalazarvasquezalexis@gmail.com>
2025-03-17 15:39:29 -0700

    Adding Somethings to Tests and Correct
    I added and removed some files to better gauge how much is supposed to be done through React. Since react is new to me I will be trying to translate my mock HTML website into react.

--- fullstack-front/src/App.js
+++ fullstack-front/src/App.js
@@ -1,18 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import ReservationPage from './pages/ReservationPage';
+import ReservationPage from './parts/ReservationPage';
 import AddUser from './pages/AddUser';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
+      {/* <mainMenu /> */}
+
       <Router>
 
       <Routes>
 
-        <Route exact path="/"element={<AddUser/>}/>
+        <Route exact path="/"element={<MainMenu/>}/>
         
       </Routes>
       </Router>
