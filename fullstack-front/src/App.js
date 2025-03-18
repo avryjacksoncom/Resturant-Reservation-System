c2f4e17d89d178e1d737704af0e31c48a8b6c40b
Robert <robertosalazarvasquezalexis@gmail.com>
2025-03-17 17:23:19 -0700

    Reorganizing and Optimizing Front-End
    So far I am catching up to Avry's Test-Bench version of the Front-End. So far I have implemented the original home page (font and background) and the Footer from the original HTML site. Similarly I have also optimized and corrected sections of the App.js/css file to better accommodate the changes that I have made. Now it is a little easier to implement other layout changes such as the planned Header, "Order Now!" section, "Reserve Seat" section, and lastly the Hero (the thing with the video in the background).

--- fullstack-front/src/App.js
+++ fullstack-front/src/App.js
@@ -1,22 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import ReservationPage from './parts/ReservationPage';
-import AddUser from './pages/AddUser';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import{ Home } from './parts/HomeSection/Home.js';
+import Footer from './parts/FooterSection/Footer.js';
+
+// import Reservation from './parts/ReservationSection';
+// import Order from './parts/OrderSect/Footer.js
+// import Header from './parts/HeaderSection';
+// import Hero from './parts/Herosection';
+
+
+// import RobertoMain from './pages/RobertoMain'
+// import ReservationPage from './pages/ReservationPage';
+// import Index from "./pages/Index";
+// import AddUser from './pages/AddUser';
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      {/* <mainMenu /> */}
-
       <Router>
 
       <Routes>
+        <Route exact path = "/" element= {<Home/>}/>
+        {/* <Route exact path = "/order" element = {<OrderSection/>}/>
+        <Route exact path = "/reservation" element = {<ReservationSection/>}/> */}
 
-        <Route exact path="/"element={<MainMenu/>}/>
-        
+        {/* <Route exact path="/page"element={<RobertoMain/>}/>
+        <Route exact path="/order" element={<RobertoOrderPage/>}/>
+        <Route exact path="/reservations" element={<ReservationPage/>}/> */}
+        {/* <Route exact path = "/add-user" element = {<AddUser/>}/> */}
+
       </Routes>
+      <Footer />
+      
       </Router>
      
     
---
+++ fullstack-front/src/parts/FooterSection/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './Footer.css';
+// import './Footer.scss' ;
+
+
+ const Footer = () => {
+    return(
+        <footer className="footer">
+            <p>
+                Created by <a href="nothingPage.html">The Worms</a>© 2025
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
